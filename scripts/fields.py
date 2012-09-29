14e2661a67251f5d2443511c0a6dd03ecf655834
Shish <shish@shishnet.org>
2012-09-29 20:15:45 +0100

    progress bar

--- scripts/fields.py
+++ scripts/fields.py
@@ -2,30 +2,32 @@
 
 import csv
 import json
+try:
+    import progressbar
+except:
+    progressbar = False
 
 ROOT="./source/"
 LEX="WDI data lexicon.csv"
 DATA="WDI_GDF_Data.csv"
 
+if progressbar: pbar = progressbar.ProgressBar().start()
+
 manual_data = file("field_descriptors.json").read()
 fields = json.loads(manual_data)
 
-#for fieldname in fields:
-#    official_name, name = line
-#    fields[official_name] = {
-#        "name": name,
-#        "official_name": official_name,
-#        "icon": name.replace(" ", "_")+".png",
-#    }
+for n, line in enumerate(csv.reader(open(ROOT+LEX))):
+    if progressbar: pbar.update(n/216.0*100)
 
-for line in csv.reader(open(ROOT+LEX)):
     idx = line[0]
     data = line[1:]
 
     if idx in fields:
         fields[idx]["description"] = str(data[0])
 
-for line in csv.reader(open(ROOT+DATA)):
+for n, line in enumerate(csv.reader(open(ROOT+DATA))):
+    if progressbar: pbar.update(n/309961.0*100)
+
     country_name = line[0]
     country_code = line[1]
     indicator_name = line[2]
@@ -43,6 +45,20 @@ for line in csv.reader(open(ROOT+DATA)):
             fields[indicator_name]["data"][country_name] = sum(data) / len(data)
         else:
             fields[indicator_name]["data"][country_name] = 0
+
+for n, indicator_name in enumerate(fields):
+    if progressbar: pbar.update(n/float(len(fields))*100)
+
+    if "data" in fields[indicator_name]:
+        in_min = min(fields[indicator_name]["data"].values())
+        in_max = max(fields[indicator_name]["data"].values())
+        in_range = in_max - in_min
+
+        for country in fields[indicator_name]["data"]:
+            orig = fields[indicator_name]["data"][country]
+            fields[indicator_name]["data"][country] = (orig - in_min) / in_range
+
+if progressbar: pbar.finish()
 
 fp = open("../data/fields.json", "w")
 #fp.write("var fields = " + json.dumps(fields, indent=4) + ";\n")
