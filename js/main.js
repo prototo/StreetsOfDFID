e52c72ef67a339386dbc16682ff79da48156e25b
Greg Mackelden <gmackelden@gmail.com>
2012-09-29 18:15:06 +0100

    updated row and flag controllers to modify one another

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(function() {
+  var flagid = 1;
+  
   /* MODELS */
   // Icon model
   IconModel = Backbone.Model.extend({
@@ -28,13 +30,26 @@ $(function() {
     },
 
     toggle : function() {
-      this.set({enabled : !this.get('enabled')});
+      var self = this,
+      enabled = !self.get('enabled');
+      self.set({enabled : enabled});
+      if (enabled) {
+        rows.add({
+          flagid : self.get('flagid')
+        });
+      } else {
+        var row = rows.find(function(row) {
+          return row.get('flagid') === self.get('flagid');
+        });
+        row.clear().remove();
+      }
     }
   });
 
   // Row model
   RowModel = Backbone.Model.extend({
     defaults : {
+      
     },
 
     initialize : function() {
@@ -86,7 +101,8 @@ $(function() {
 
     toggle : function() {
       this.model.toggle();
-      this.$el.toggleClass('enabled', this.model.get('enabled'));
+      var enabled = this.model.get('enabled');
+      this.$el.toggleClass('enabled', enabled);
     }
   });
 
@@ -101,37 +117,46 @@ $(function() {
     
     initialize : function() {
       this.$el.css('background-image', 'url('+this.model.get('src')+')');
+      this.model.bind('change', this.render, this);
     },
 
     render : function() {
+      var enabled = this.model.get('enabled');
+      this.$el.toggleClass('enabled', enabled);
       return this;
     },
 
     toggle : function() {
       this.model.toggle();
-      this.$el.toggleClass('enabled', this.model.get('enabled'));
     }
   });
 
   // Row view
   RowView = Backbone.View.extend({
     tagName : 'span',
+    className : 'row',
+    template : _.template($('#row-template').html()),
 
     events : {
-      'click .remove' : 'remove'
+      'click .remove' : 'clear'
     },
 
     initialize : function() {
-      this.model.bind('change', this.render, this);
       this.model.bind('remove', this.remove, this);
+      this.model.bind('change', this.render, this);
     },
 
     render : function() {
-      
+      this.$el.html( this.template( this.model.toJSON() ) );
+      return this;
     },
 
-    remove : function() {
-      this.model.remove();
+    clear : function() {
+      var flagid = this.model.get('flagid'),
+      flag = flags.find(function(flag){
+        return flag.get('flagid') === flagid;
+      });
+      flag.toggle();
     }
   });
   
@@ -142,8 +167,10 @@ $(function() {
     initialize : function() {
       icons.on('add', this.addIcon, this);
       flags.on('add', this.addFlag, this);
+      rows.on('add', this.addRow, this);
 
-      _.each(_.range(10), function() { icons.add({}); flags.add({}); });
+      // fake fill the icon and flag lists
+      _.each(_.range(10), function() { icons.add({}); flags.add({flagid:flagid++}); });
     },
 
     render : function() {
@@ -158,6 +185,11 @@ $(function() {
     addFlag : function(flag) {
       var view = new FlagView({model : flag});
       $('#left-bar').append( view.render().el );
+    },
+    
+    addRow : function(row) {
+      var view = new RowView({model : row});
+      $('#rows').append( view.render().el );
     }
   });
 
